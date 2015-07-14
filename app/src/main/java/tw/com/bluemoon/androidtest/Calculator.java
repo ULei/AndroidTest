816ed9b765640b3d4b073481c372f23bbafaf124
Chris Jeng <joanneandy1479@gmail.com>
2015-07-14 21:03:16 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/bluemoon/androidtest/Calculator.java
@@ -0,0 +1,23 @@
+package tw.com.bluemoon.androidtest;
+
+/**
+ * Created by Chris on 2015/7/2.
+ */
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double substract(double a, double b){
+        return a - b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+}
---
+++ app/src/test/java/tw/com/bluemoon/androidtest/CalculatorTest.java
@@ -0,0 +1,35 @@
+package tw.com.bluemoon.androidtest;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Chris on 2015/7/14.
+ */
+public class CalculatorTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSum() throws Exception {
+
+    }
+
+    @Test
+    public void testSubstract() throws Exception {
+
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+}
